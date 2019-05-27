9846c191603c2f54082252ed54a173289f4c6170
Tammy Metz <tammy@tammymetz.com>
2019-05-27 11:28:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,9 @@ const logDriversByHometown = function(drivers, location) {
     }
   });
 };
+
+const driversByRevenue = function(drivers) {
+  drivers.sort(function(driver1, driver2)) {
+    return driver1.revenue - driver2.revenue;
+  }
+}

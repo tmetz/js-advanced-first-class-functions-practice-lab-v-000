41bf4a01b75f3f937c342b352e45e5f47f38116f
Tammy Metz <tammy@tammymetz.com>
2019-05-27 11:43:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,10 +23,9 @@ const driversByRevenue = function(drivers) {
 };
 
 const driversByName = function(drivers) {
-  drivers.slice().sort(function(driver1, driver2) {
+  return drivers.slice().sort(function(driver1, driver2) {
     return driver1.name.localeCompare(driver2.name);
   });
-  return drivers;
 };
 
 const totalRevenue = function(drivers) {

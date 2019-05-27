f1ad5f5b9b860c675acda5022977db07ac04b369
Tammy Metz <tammy@tammymetz.com>
2019-05-27 11:48:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,3 +34,7 @@ const totalRevenue = function(drivers) {
     return total + currentDriver.revenue;
   }, 0);
 }
+
+const averageRevenue = function(drivers) {
+  return totalRevenue(drivers) / drivers.length;
+}
